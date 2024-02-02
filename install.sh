d57d1e568933738f0aff943d27a579d0b94897bb
Kaveen Lakmuthu <139112868+kaveen-lakmuthu@users.noreply.github.com>
2024-02-02 22:25:53 +0530

    Update and rename test.sh to install.sh
    Still doesn't work

--- install.sh
+++ install.sh
@@ -11,23 +11,25 @@ builddir=$(pwd)
 pacman -Syu
 
 cd $builddir
-pacman -S git --needed base-devel
+pacman -S qtile git --needed base-devel
 
 git clone https://github.com/kaveen-lakmuthu/qtile-config.git
 mv qtile-config/ qtile/
 
-mkdir -p /home/$username/.config
+mkdir -p /home/$username/.config/qtile
+cp qtile /home/$username/.config/
 
-cp qtile/ /home/$username/.config/
+mv user-dirs.dirs /home/$username/.config
+chown -R $username:$username /home/$username
+
 chmod -x /home/$username/.config/qtile/autostart_once.sh
 chmod -X /home/$username/.config/qtile/config.py
 
-
 pacman -S xorg xorg-xinit xorg-server -y
-pacman -S alacritty firefox nitrogen polkit volumeicon dunst picom lxsession redshift blueman-applet xfce4-power-manager light-locker flameshot network-manager-applet ttf-font-awesome lightdm -y
+pacman -S alacritty firefox nitrogen polkit volumeicon dunst picon lxsession redshift blueman xfce4-power-manager light-locker flameshot network-manager-applet ttf-font-awesome lightdm -y
 pacman -S thunar neofetch htop dmenu unzip wget vim -y
 
-
+touch ~/.xinitrc
 echo "exec qtile start"  > ~/.xinitrc
 
 systemctl enable lightdm
